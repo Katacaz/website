d7d31197da5c2e2afa9f34ba8470d03e7afd4174
Zac Docherty <zacdoc@hotmail.com>
2024-06-22 23:47:43 +1000

    Attempted router fix (change to HashRouter)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
@@ -11,20 +11,18 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<AboutMe />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/content" element={<ContentCreation />} />
-            <Route path="/blog" element={<ReviewBlog />} />
-          </Routes>
-        </div>
+    <div className="App">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/content" element={<ContentCreation />} />
+          <Route path="/blog" element={<ReviewBlog />} />
+        </Routes>
       </div>
-    </Router>
+    </div>
   );
 }
 
